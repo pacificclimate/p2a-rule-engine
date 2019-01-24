2ba81141c0a919ecf6ca7ceea046b7fc99c72ccf
Nikola Rados <nrados95@gmail.com>
2019-01-24 14:26:33 -0800

    Add some basic http requests to CE

--- scripts/data_acquisition.py
+++ scripts/data_acquisition.py
@@ -1,5 +1,8 @@
 import csv
 import json
+import requests
+from statistics import mean
+
 
 
 def get_val_from_dict(dict, val):
@@ -30,3 +33,56 @@ def read_csv(filename):
             rules_dict[rule] = cond
 
     return rules_dict
+
+
+def get_val_from_keys(stats, value, date):
+    for key in stats.keys():
+        if date in key:
+            return stats[key][value]
+    # no match found
+    return None
+
+
+def build_url_params(time, variable, timescale):
+    return {'ensemble_name': 'ce_files',  # hard code for now
+            'model': 'CanESM2',           # i think the website has a fixed model?
+            'emission': 'historical,rcp85',   # fixed??
+            'time': time,
+            'area': None,                 # this will be determined by website
+            'variable': variable,
+            'timescale': timescale}
+
+
+def get_data_from_ce(var_name):
+    print(var_name)
+    # hard coded for now
+    date = '20100101-20391231'
+
+    # lookup tables for known variables
+    var_opt = {'temp': {'min': 'tasmin', 'max': 'tasmax'},
+               'prec': 'pr'}
+    p_opt = {'djf': {'time': 0, 'timescale': 'seasonal'},
+             'mam': {'time': 1, 'timescale': 'seasonal'},
+             'jja': {'time': 2, 'timescale': 'seasonal'},
+             'son': {'time': 3, 'timescale': 'seasonal'},
+             'ann': {'time': 0, 'timescale': 'yearly'}}
+    val_opt = {'s0p': 'min',
+               's100p': 'max',
+               'smean': 'mean'}
+
+    # endpoint + api we will use
+    request_type = 'multistats'
+    url_endpoint = 'https://services.pacificclimate.org/marmot/api/{}'.format(request_type)
+
+    # deconstruct variable name
+    var, toy, ia_agg, spat_agg, enstime = var_name.split('_')
+
+    if var == 'prec':
+        ret = get_val_from_keys(requests.get(url_endpoint, params=build_url_params(p_opt[toy]['time'], var_opt[var], p_opt[toy]['timescale'])).json(), val_opt[spat_agg], date)
+        print(ret)
+        return ret
+    elif var == 'temp':
+        ret = mean([get_val_from_keys(requests.get(url_endpoint, params=build_url_params(p_opt[toy]['time'], var_opt[var]['max'], p_opt[toy]['timescale'])).json(), val_opt[spat_agg], date),
+                    get_val_from_keys(requests.get(url_endpoint, params=build_url_params(p_opt[toy]['time'], var_opt[var]['min'], p_opt[toy]['timescale'])).json(), val_opt[spat_agg], date)])
+        print(ret)
+        return ret
