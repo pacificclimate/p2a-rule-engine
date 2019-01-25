1482f3750ed6df854dcf184752cfed1dd54793ef
Nikola Rados <nrados95@gmail.com>
2019-01-25 13:19:04 -0800

    Extra case for region check

--- scripts/data_acquisition.py
+++ scripts/data_acquisition.py
@@ -4,6 +4,37 @@ import requests
 from statistics import mean
 
 
+variable_options = {
+    'temp': {'min': 'tasmin', 'max': 'tasmax'},
+    'prec': 'pr'
+}
+
+time_of_year_options = {
+    'ann': {'time': 0, 'timescale': 'yearly'},
+    'djf': {'time': 0, 'timescale': 'seasonal'},
+    'mam': {'time': 1, 'timescale': 'seasonal'},
+    'jja': {'time': 2, 'timescale': 'seasonal'},
+    'son': {'time': 3, 'timescale': 'seasonal'},
+    'jan': {'time': 0, 'timescale': 'monthly'},
+    'feb': {'time': 1, 'timescale': 'monthly'},
+    'mar': {'time': 2, 'timescale': 'monthly'},
+    'apr': {'time': 3, 'timescale': 'monthly'},
+    'may': {'time': 4, 'timescale': 'monthly'},
+    'jun': {'time': 5, 'timescale': 'monthly'},
+    'jul': {'time': 6, 'timescale': 'monthly'},
+    'aug': {'time': 7, 'timescale': 'monthly'},
+    'sep': {'time': 8, 'timescale': 'monthly'},
+    'oct': {'time': 9, 'timescale': 'monthly'},
+    'nov': {'time': 10, 'timescale': 'monthly'},
+    'dec': {'time': 11, 'timescale': 'monthly'},
+}
+
+spat_agg_options = {
+    's0p': 'min',
+    's100p': 'max',
+    'smean': 'mean'
+}
+
 
 def get_val_from_dict(dict, val):
     """Given a dictionary key name return the associated value"""
@@ -36,6 +67,9 @@ def read_csv(filename):
 
 
 def get_val_from_keys(stats, value, date):
+    """Given dictionary search for a particular date in one of the keys and
+       and retrieve the value of the variable at that date.
+    """
     for key in stats.keys():
         if date in key:
             return stats[key][value]
@@ -44,6 +78,7 @@ def get_val_from_keys(stats, value, date):
 
 
 def build_url_params(time, variable, timescale):
+    """Add parameters to dictionary representation of query string"""
     return {'ensemble_name': 'ce_files',  # hard code for now
             'model': 'CanESM2',           # i think the website has a fixed model?
             'emission': 'historical,rcp85',   # fixed??
@@ -53,36 +88,58 @@ def build_url_params(time, variable, timescale):
             'timescale': timescale}
 
 
-def get_data_from_ce(var_name):
-    print(var_name)
+def get_ce_data(var_name):
+    """Parse a given variable name into parameters that are used to query the
+       Climate Explorer backend.
+    """
     # hard coded for now
     date = '20100101-20391231'
 
-    # lookup tables for known variables
-    var_opt = {'temp': {'min': 'tasmin', 'max': 'tasmax'},
-               'prec': 'pr'}
-    p_opt = {'djf': {'time': 0, 'timescale': 'seasonal'},
-             'mam': {'time': 1, 'timescale': 'seasonal'},
-             'jja': {'time': 2, 'timescale': 'seasonal'},
-             'son': {'time': 3, 'timescale': 'seasonal'},
-             'ann': {'time': 0, 'timescale': 'yearly'}}
-    val_opt = {'s0p': 'min',
-               's100p': 'max',
-               'smean': 'mean'}
-
     # endpoint + api we will use
     request_type = 'multistats'
-    url_endpoint = 'https://services.pacificclimate.org/marmot/api/{}'.format(request_type)
+    url_endpoint = 'https://services.pacificclimate.org/marmot/api/{}'.format(
+        request_type)
 
     # deconstruct variable name
     var, toy, ia_agg, spat_agg, enstime = var_name.split('_')
 
     if var == 'prec':
-        ret = get_val_from_keys(requests.get(url_endpoint, params=build_url_params(p_opt[toy]['time'], var_opt[var], p_opt[toy]['timescale'])).json(), val_opt[spat_agg], date)
-        print(ret)
-        return ret
+        return get_val_from_keys(
+            requests.get(
+                url_endpoint,
+                params=build_url_params(
+                    time_of_year_options[toy]['time'],
+                    variable_options[var],
+                    time_of_year_options[toy]['timescale'])).json(),
+            spat_agg_options[spat_agg],
+            date)
     elif var == 'temp':
-        ret = mean([get_val_from_keys(requests.get(url_endpoint, params=build_url_params(p_opt[toy]['time'], var_opt[var]['max'], p_opt[toy]['timescale'])).json(), val_opt[spat_agg], date),
-                    get_val_from_keys(requests.get(url_endpoint, params=build_url_params(p_opt[toy]['time'], var_opt[var]['min'], p_opt[toy]['timescale'])).json(), val_opt[spat_agg], date)])
-        print(ret)
-        return ret
+        return mean([
+            get_val_from_keys(
+                requests.get(
+                    url_endpoint,
+                    params=build_url_params(
+                        time_of_year_options[toy]['time'],
+                        variable_options[var]['max'],
+                        time_of_year_options[toy]['timescale'])).json(),
+                spat_agg_options[spat_agg],
+                date),
+            get_val_from_keys(
+                requests.get(
+                    url_endpoint,
+                    params=build_url_params(
+                        time_of_year_options[toy]['time'],
+                        variable_options[var]['min'],
+                        time_of_year_options[toy]['timescale'])).json(),
+                spat_agg_options[spat_agg],
+                date)])
+
+
+def get_variables(var_name):
+    """Given a variable name return the value by lookup table or by querying
+       the CE backend.
+    """
+    if var_name == 'region_oncoast':
+        return 1
+    else:
+        return get_ce_data(var_name)
