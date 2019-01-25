8932fc7bece671ff85bf7de80fd29fbd6e1c4bb2
Nikola Rados <nrados95@gmail.com>
2019-01-25 13:20:35 -0800

    Fix bad variable/method names

--- scripts/main.py
+++ scripts/main.py
@@ -3,7 +3,8 @@ from functools import partial
 
 from resolver import build_parse_tree
 from evaluator import evaluate_rule
-from data_acquisition import get_val_from_dict, get_json_var, read_csv
+from data_acquisition import get_val_from_dict, get_json_var, read_csv, \
+    get_variables
 
 
 def print_dict(d):
@@ -20,37 +21,36 @@ def main():
     args = parser.parse_args()
 
     # read csv
-    rules_dict = read_csv(args.csv)
+    rules = read_csv(args.csv)
 
     # create parse tree dictionary
-    parse_tree_dict = {}
-    variable_set = set()
-    for rule, condition in rules_dict.items():
+    parse_trees = {}
+    variables = set()
+    for rule, condition in rules.items():
         try:
-            parse_tree_dict[rule], vars = build_parse_tree(condition)
+            parse_trees[rule], vars = build_parse_tree(condition)
         except SyntaxError as e:
             print('{}, rule will be excluded'.format(e))
 
         # add unique variables to set
         for var in vars:
-            variable_set.add(var)
+            variables.add(var)
 
     # get values for all variables we will need for evaluation
-    variable_dict = {}
-    for var in variable_set:
-        variable_dict[var] = get_data_from_ce(var)
+    collected_variables = {}
+    for var in variables:
+        collected_variables[var] = get_variables(var)
 
     # partially define dict accessor to abstract it for the evaluator
-    variable_getter = partial(get_val_from_dict, variable_dict)
+    variable_getter = partial(get_val_from_dict, collected_variables)
 
     # evaluate parse trees
-    result_dict = {}
-    for rule, pt in parse_tree_dict.items():
-        result_dict[rule] = evaluate_rule(pt, parse_tree_dict,
-                                                variable_getter)
+    result = {}
+    for id, rule in parse_trees.items():
+        result[id] = evaluate_rule(rule, parse_trees, variable_getter)
 
     # print for now
-    print_dict(result_dict)
+    print_dict(result)
 
 
 if __name__ == '__main__':
