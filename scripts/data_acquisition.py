d6e585b5663fc788c29b42233d72297e52a06a3d
Nikola Rados <nrados95@gmail.com>
2019-01-28 12:56:03 -0800

    Reduce number of variable cases

--- scripts/data_acquisition.py
+++ scripts/data_acquisition.py
@@ -33,10 +33,16 @@ time_of_year_options = {
     'dec': {'time': 11, 'timescale': 'monthly'},
 }
 
-spat_agg_options = {
+data_options = {
     's0p': 'min',
     's100p': 'max',
     'smean': 'mean'
+}
+
+date_options = {
+    '2020s': ['20100101-20391231', '20110101-20400101'],
+    '2050s': ['20400101-20691231'],
+    '2080s': ['20700101-20991231']
 }
 
 
@@ -45,139 +51,86 @@ def get_val_from_dict(dict, val):
     return dict[val]
 
 
-def get_json_var(filename, var):
-    """Given a variable name return the associated value from a json file"""
-    data = ''
-    with open(filename) as json_data:
-        data = json.load(json_data)
-    return data[var]
-
-
 def read_csv(filename):
     """Read a csv file which contains at least the id and condition columns
        and return those columns applying a 'rule_' prefix to the id column.
     """
-    rules_dict = {}
-    with open(filename, 'r') as rules_file:
-        csv_reader = list(csv.DictReader(rules_file, delimiter=';'))
+    rules = {}
+    with open(filename, 'r') as f:
+        csv_reader = list(csv.DictReader(f, delimiter=';'))
         for row in csv_reader:
-            # make sure we are only getting the id and condition
-            # add rule prefix
             rule = 'rule_{}'.format(list(row.values())[0])
             cond = list(row.values())[1]
-            rules_dict[rule] = cond
+            rules[rule] = cond
 
-    return rules_dict
+    return rules
 
 
-def get_val_from_keys(stats, value, date):
-    """Given dictionary search for a particular date in one of the keys and
-       and retrieve the value of the variable at that date.
+def get_period_data(target, date_range, periods):
+    """Given a json object containing data for different 30 year periods,
+       find the desired period and return the target variable.
     """
-    for key in stats.keys():
-        if date in key:
-            return stats[key][value]
-    # no match found
+    dates = date_options[date_range]
+    for key in periods.keys():
+        for date in dates:
+            if date in key:
+                return periods[key][target]
+
+    print('Range {} could not be found in {}'.format(range, periods))
     return None
 
 
-def build_url_params(time, variable, timescale):
-    """Add parameters to dictionary representation of query string"""
-    return {'ensemble_name': 'ce_files',  # hard code for now
-            'model': 'CanESM2',           # i think the website has a fixed model?
-            'emission': 'historical,rcp85',   # fixed??
-            'time': time,
-            'area': None,                 # this will be determined by website
-            'variable': variable,
-            'timescale': timescale}
-
-
-def get_ce_data(var_name):
-    """Parse a given variable name into parameters that are used to query the
-       Climate Explorer backend.
+def request_data(variable, time, timescale):
+    """Request data from the Climate Explorer backend using multistats and
+       return a json object containing data from all the 30 year periods (2020s,
+       2050s, 2080s)
     """
-    # hard coded for now
-    date = '20100101-20391231'
+    url = 'https://services.pacificclimate.org/marmot/api/multistats'
+    query_string = {
+        'ensemble_name': 'ce_files',
+        'model': 'CanESM2',             # fixed model?
+        'emission': 'historical,rcp85', # fixed emission scenario?
+        'time': time,
+        'area': None,                   # polygon passed in by p2a?
+        'variable': variable,
+        'timescale': timescale}
 
-    # endpoint
-    url_endpoint = 'https://services.pacificclimate.org/marmot/api/'
+    return requests.get(url, params=query_string).json()
 
-    # deconstruct variable name
-    var, toy, ia_agg, spat_agg, enstime = var_name.split('_')
 
-    if var == 'prec':
-        return get_val_from_keys(
-            requests.get(
-                url_endpoint + 'multistats',
-                params=build_url_params(
-                    time_of_year_options[toy]['time'],
-                    variable_options[var],
-                    time_of_year_options[toy]['timescale'])).json(),
-            spat_agg_options[spat_agg],
-            date)
-    elif var == 'temp':
-        return mean([
-            get_val_from_keys(
-                requests.get(
-                    url_endpoint + 'multistats',
-                    params=build_url_params(
-                        time_of_year_options[toy]['time'],
-                        variable_options[var]['max'],
-                        time_of_year_options[toy]['timescale'])).json(),
-                spat_agg_options[spat_agg],
-                date),
-            get_val_from_keys(
-                requests.get(
-                    url_endpoint + 'multistats',
-                    params=build_url_params(
-                        time_of_year_options[toy]['time'],
-                        variable_options[var]['mean'],
-                        time_of_year_options[toy]['timescale'])).json(),
-                spat_agg_options[spat_agg],
-                date)])
-    elif var == 'dg05':
-        return get_val_from_keys(
-            requests.get(
-                url_endpoint + 'multistats',
-                params=build_url_params(
-                    0,
-                    variable_options[var],
-                    'yearly')).json(),
-                spat_agg_options[spat_agg],
-                '20110101-20400101')
-    # elif var == 'nffd':
-    #     print(
-    #         requests.get(
-    #             url_endpoint + 'multistats',
-    #             params=build_url_params(
-    #                 0,
-    #                 'fdETCCDI',
-    #                 'yearly')).json())
-    # elif var == 'pass':
-    #     print(
-    #         requests.get(
-    #             url_endpoint + 'multistats',
-    #             params=build_url_params(
-    #                 0,
-    #                 'fdETCCDI',
-    #                 'yearly')).json())
-    elif var == 'dl18':
-        return get_val_from_keys(
-            requests.get(
-                url_endpoint + 'multistats',
-                params=build_url_params(
-                    0,
-                    variable_options[var],
-                    'yearly')).json(),
-                spat_agg_options[spat_agg],
-                '20110101-20400101')
-                
+def get_ce_data(var_name, date_range):
+    """Parse a given variable name and into parameters that are used to query
+       the Climate Explorer backend.
+    """
+    variable, time_of_year, inter_annual_var, data, percentile = var_name.split('_')
 
-def get_variables(var_name):
+    if variable == 'temp':
+        # special case for temp because we have to use tasmin and tasmax together
+        tasmin = get_period_data(data_options[data], date_range,
+                    request_data(
+                        variable_options[variable]['min'],
+                        time_of_year_options[time_of_year]['time'],
+                        time_of_year_options[time_of_year]['timescale']))
+        tasmax = get_period_data(data_options[data], date_range,
+                    request_data(
+                        variable_options[variable]['max'],
+                        time_of_year_options[time_of_year]['time'],
+                        time_of_year_options[time_of_year]['timescale']))
+        return mean([tasmin, tasmax])
+
+    else:
+        return get_period_data(data_options[data], date_range,
+                    request_data(
+                        variable_options[variable],
+                        time_of_year_options[time_of_year]['time'],
+                        time_of_year_options[time_of_year]['timescale']))
+
+
+def get_variables(var_name, date_range):
     """Given a variable name return the value by lookup table or by querying
        the CE backend.
     """
-    if var_name == 'region_oncoast':
+    if var_name == 'region_oncoast':    # temp fix
         return 1
     else:
-        return get_ce_data(var_name)
+        return get_ce_data(var_name, date_range)
