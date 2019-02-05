e1549e7c94f361ec8bf43fedb6f0513014ab74ec
Nikola Rados <nrados95@gmail.com>
2019-02-05 11:44:24 -0800

    Add sesh

--- scripts/main.py
+++ scripts/main.py
@@ -1,5 +1,7 @@
 from argparse import ArgumentParser
 from functools import partial
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from resolver import build_parse_tree
 from evaluator import evaluate_rule
@@ -56,7 +58,9 @@ def main():
         region = None
 
     # get values for all variables we will need for evaluation
-    collected_variables = {var: get_variables(var, args.date_range, region) for var in variables}
+    Session = sessionmaker(create_engine('postgres://ce_meta_ro@db3.pcic.uvic.ca/ce_meta'))
+    sesh = Session()
+    collected_variables = {var: get_variables(sesh, var, args.date_range, region) for var in variables}
 
     # partially define dict accessor to abstract it for the evaluator
     variable_getter = partial(get_val_from_dict, collected_variables)
